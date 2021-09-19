aa7ccbdfe2645854ca5ecab0d9d62854fd99721d
trevdso <dsouzatrevor97@gmail.com>
2021-09-19 19:03:32 -0400

    Added Tailwind.css to the project Allows for utility css classes and reduces dev time

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Test app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={tw`flex-1 bg-white items-center justify-center`}>
+      <View>
+        <Text>Test app</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
