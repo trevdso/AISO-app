e557442eb7ccd2b82a681c0161b18f93710fb859
trevdso <dsouzatrevor97@gmail.com>
2021-09-20 00:29:10 -0400

    Uniform status bar for ios and android

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { SafeAreaView, Text, View, Alert } from "react-native";
+import {
+  StatusBar,
+  SafeAreaView,
+  Text,
+  View,
+  Alert,
+  Platform,
+} from "react-native";
 import Button from "./src/components/Button/Button";
 import tw from "./lib/tailwind";
 
 const App = () => {
+  StatusBar.setBarStyle("light-content", true);
+  Platform.OS === "android" && StatusBar.setTranslucent(true);
   return (
     <SafeAreaView style={tw`flex-1 items-center justify-center bg-aiso-gray`}>
+      <StatusBar style={tw`text-white`} />
       <View style={tw`bg-aiso-gray`}>
         <Text style={tw`text-white`}>Test app</Text>
         <Button title="Test!" onPress={() => Alert.alert("Clicked!")} />
-
-        <StatusBar style="auto" />
       </View>
     </SafeAreaView>
   );
