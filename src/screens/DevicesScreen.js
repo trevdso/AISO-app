368d2258b219e11b8bcf60a22e2c59cc67a1b7c5
trevdso <dsouzatrevor97@gmail.com>
2021-10-15 16:21:38 -0400

    initial attempt at socketio

--- src/screens/DevicesScreen.js
+++ src/screens/DevicesScreen.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View, Alert } from "react-native";
 import Button from "../components/Button/Button";
+
+//import { io } from "socket.io-client";
+
 import tw from "../../lib/tailwind";
 
 const DevicesScreen = () => {
+  //const socket = io();
   return (
     <View style={tw`flex-1 items-center justify-center`}>
       <Text style={tw`text-white`}>Test app</Text>
