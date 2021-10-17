3fe3b5a05c013c9c91450013c139eb3c6be5a2a2
Trevor D'Souza <dsouzatrevor97@gmail.com>
2021-10-17 18:32:54 -0400

    Socketio implementation

--- src/screens/DevicesScreen.js
+++ src/screens/DevicesScreen.js
@@ -1,17 +1,72 @@
-import React from "react";
-import { StyleSheet, Text, View, Alert } from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { StyleSheet, Text, View, Alert, TextInput } from "react-native";
 import Button from "../components/Button/Button";
 
-//import { io } from "socket.io-client";
+import { io } from "socket.io-client";
 
 import tw from "../../lib/tailwind";
 
+const socket = io("http://localhost:3000/");
+
 const DevicesScreen = () => {
-  //const socket = io();
+  const [input, setInput] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+  const [allDevices, setAllDevices] = useState([]);
+
+  const sendMessage = () => {
+    console.log(input);
+    setAllMessages((oldMessages) => [...oldMessages, input]);
+    console.log(allMessages);
+    socket.emit("chat message", input);
+    setInput("");
+  };
+
+  useEffect(() => {
+    socket.on("chat message", (msg) => {
+      setAllMessages((oldMessages) => [...oldMessages, msg]);
+      console.log(msg);
+      console.log(allMessages);
+    });
+    socket.on("getAllOnlineDevices", (allDevices) => {
+      setAllDevices([...allDevices]);
+      console.log(allDevices);
+    });
+
+    socket.emit("getAllOnlineDevices");
+    return () => {
+      socket.off("chat message");
+      socket.off("getAllDevices");
+    };
+  }, []);
+
   return (
     <View style={tw`flex-1 items-center justify-center`}>
-      <Text style={tw`text-white`}>Test app</Text>
-      <Button title="Test!" onPress={() => Alert.alert("Clicked!")} />
+      {allDevices.map((device, index) => {
+        return (
+          <View>
+            <Text key={index} style={tw`text-white`}>
+              {device}
+            </Text>
+          </View>
+        );
+      })}
+      {allMessages.map((msg, index) => {
+        return (
+          <View>
+            <Text key={index} style={tw`text-white`}>
+              {msg}
+            </Text>
+          </View>
+        );
+      })}
+      <TextInput
+        style={{ height: 40, borderWidth: 2, top: 600, color: "white" }}
+        autoCorrect={false}
+        value={input}
+        onSubmitEditing={sendMessage}
+        onChangeText={setInput}
+      />
+      <Button title="Test!" onPress={sendMessage} />
     </View>
   );
 };
