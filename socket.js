156e77a9949a2fb1af7e103ca55347b8b35da985
trevdso <dsouzatrevor97@gmail.com>
2021-10-28 11:14:11 -0400

    Changed url to use deployed server

--- socket.js
+++ socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3000/", { autoConnect: false });
+const socket = io("https://aiso-server.herokuapp.com/", {
+  autoConnect: false,
+});
 
 socket.onAny((event, ...args) => {
   console.log(event, args);
