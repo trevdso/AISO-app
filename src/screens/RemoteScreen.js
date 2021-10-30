b764abca847dccffec06cd0ae37ec06d7a90ae64
trevdso <dsouzatrevor97@gmail.com>
2021-10-29 23:53:29 -0400

    Added actions for remote buttons Changed pickup button to hand icon

--- src/screens/RemoteScreen.js
+++ src/screens/RemoteScreen.js
@@ -10,6 +10,7 @@ import socket from "../../socket";
 const RemoteScreen = ({ navigation, route }) => {
   const [disabled, setDisabled] = useState(true);
   const [device, setDevice] = useState(null);
+  const [pickedUp, setPickedUp] = useState(false);
 
   const disconnectBot = (currentDevice) => {
     if (currentDevice != null) {
@@ -22,6 +23,34 @@ const RemoteScreen = ({ navigation, route }) => {
     }
   };
 
+  const performAction = (action, currentDevice) => {
+    switch (action) {
+      case "up":
+        socket.emit("move-up", currentDevice.id);
+        break;
+      case "down":
+        socket.emit("move-down", currentDevice.id);
+        break;
+      case "left":
+        socket.emit("move-left", currentDevice.id);
+        break;
+      case "right":
+        socket.emit("move-right", currentDevice.id);
+        break;
+      case "pickup":
+        pickedUp
+          ? socket.emit("put-down", currentDevice.id)
+          : socket.emit("pick-up", currentDevice.id);
+        setPickedUp(!pickedUp);
+        break;
+      case "estop":
+        socket.emit("estop", currentDevice.id);
+        break;
+      default:
+        break;
+    }
+  };
+
   useFocusEffect(
     useCallback(() => {
       setDevice(route.params ? route.params.device : null);
@@ -30,7 +59,7 @@ const RemoteScreen = ({ navigation, route }) => {
         route.params = null;
         setDevice(null);
       });
-    }, [route, disabled, device])
+    }, [route, disabled, device, pickedUp])
   );
   return (
     <View>
@@ -46,14 +75,34 @@ const RemoteScreen = ({ navigation, route }) => {
         )}
       </View>
       <View style={tw`flex items-center justify-evenly px-2 py-2 h-80`}>
-        <PadButton icon={"up"} disabled={disabled} />
+        <PadButton
+          icon={"up"}
+          disabled={disabled}
+          onPress={() => performAction("up", device)}
+        />
         <View style={tw`flex-row items-center justify-evenly px-2 py-2 w-100`}>
-          <PadButton icon={"left"} disabled={disabled} />
-          <PadButton icon={"default"} disabled={disabled} />
-          <PadButton icon={"right"} disabled={disabled} />
+          <PadButton
+            icon={"left"}
+            disabled={disabled}
+            onPress={() => performAction("left", device)}
+          />
+          <PadButton
+            icon={"pickup"}
+            disabled={disabled}
+            onPress={() => performAction("pickup", device)}
+          />
+          <PadButton
+            icon={"right"}
+            disabled={disabled}
+            onPress={() => performAction("right", device)}
+          />
         </View>
 
-        <PadButton icon={"down"} disabled={disabled} />
+        <PadButton
+          icon={"down"}
+          disabled={disabled}
+          onPress={() => performAction("down", device)}
+        />
       </View>
       <View style={tw`flex items-center justify-around px-2 py-2 h-80`}>
         <View style={tw`flex-row items-end justify-evenly px-2 py-2 w-100`}>
@@ -62,7 +111,11 @@ const RemoteScreen = ({ navigation, route }) => {
             disabled={disabled}
             onPress={() => disconnectBot(device)}
           />
-          <PadButton icon={"stop"} disabled={disabled} />
+          <PadButton
+            icon={"stop"}
+            disabled={disabled}
+            onPress={() => performAction("estop", device)}
+          />
         </View>
       </View>
     </View>
